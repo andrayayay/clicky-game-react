b7dfa2409a4c84e62ca1ac1fc0f34aae2959527f
Andrayayay <andrayayay@gmail.com>
2019-05-06 16:23:51 -0500

    update - still needs to be responsive

--- src/App.js
+++ src/App.js
@@ -22,10 +22,12 @@ class App extends React.Component {
         console.log(this.state.highscore);
       });
     }
+    // setting that we want each card to only be clicked once
     this.state.characters.forEach(card => {
       card.count = 0;
     });
-    alert(`Game Over :( \nscore: ${this.state.score}`);
+    // alert game over and set score back to 0
+    alert(`Game Over! Your score is: ${this.state.score} points `);
     this.setState({score: 0});
     return true;
   }
@@ -34,11 +36,13 @@ class App extends React.Component {
     // eslint-disable-next-line 
     this.state.characters.find((o, i) => {
       if (o.id === id) {
+        // if character count equal 0, add one point to score
         if(characters[i].count === 0){
           characters[i].count = characters[i].count + 1;
           this.setState({score : this.state.score + 1}, function(){
             console.log(this.state.score);
           });
+          // rearrange character photos
           this.state.characters.sort(() => Math.random() - 0.5)
           return true;
         } else {
@@ -51,7 +55,7 @@ class App extends React.Component {
   render() {
     return (
       <Wrapper>
-        <Container score={this.state.score} highscore={this.state.highscore}>Avengers Memory Game</Container>
+        <Container score={this.state.score} highscore={this.state.highscore}></Container>
         {this.state.characters.map(card => (
           <Card
             id={card.id}
