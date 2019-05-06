13062773d2211826bdeca96c4b26f223fffdf92a
Andrayayay <andrayayay@gmail.com>
2019-05-06 16:41:03 -0500

    Updates

---
+++ precache-manifest.d5df5bbafd2748d6ea1c3e07a131c65a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c0a6c1e285a171ac85f64799d098621",
+    "url": "/clicky-game-react/index.html"
+  },
+  {
+    "revision": "5f891e37b3e1c8db638c",
+    "url": "/clicky-game-react/static/css/main.5c194b3a.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/clicky-game-react/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "5f891e37b3e1c8db638c",
+    "url": "/clicky-game-react/static/js/main.c0032cc5.chunk.js"
+  },
+  {
+    "revision": "96e9a9b550dae971e8fe",
+    "url": "/clicky-game-react/static/js/runtime~main.4ef103c0.js"
+  }
+]);
